2b9d91809b0f8edd3a91e517a91fd3f400117d2a
Kirill <timofeev.kirill.a@gmail.com>
2016-05-05 19:55:28 +0300

    key annotations parser add

--- src/im/kirillt/yandexkinopoisk/DAO/annotations/parser/AnnotationsParser.java
+++ src/im/kirillt/yandexkinopoisk/DAO/annotations/parser/AnnotationsParser.java
@@ -1,7 +1,13 @@
 package im.kirillt.yandexkinopoisk.DAO.annotations.parser;
 
+import im.kirillt.yandexkinopoisk.DAO.annotations.Key;
 import im.kirillt.yandexkinopoisk.DAO.annotations.Table;
+import im.kirillt.yandexkinopoisk.DAO.annotations.parser.exceptions.NoKeysFoundException;
 import im.kirillt.yandexkinopoisk.DAO.annotations.parser.exceptions.NotATableException;
+
+import java.lang.reflect.Field;
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class AnnotationsParser {
@@ -10,5 +16,16 @@ public class AnnotationsParser {
             return clazz.getAnnotation(Table.class).name();
         }
         throw new NotATableException(clazz);
+    }
+
+    public static Map<String, Object> getKeys(Class<?> clazz) throws NoKeysFoundException, IllegalAccessException {
+        final Map<String, Object> keys = new HashMap<>();
+        final Field[] fields = clazz.getDeclaredFields();
+        for (Field field : fields) {
+            if (field.isAnnotationPresent(Key.class)) {
+                keys.put(field.getName(), field.get(clazz));
+            }
+        }
+        return keys;
     }
 }
