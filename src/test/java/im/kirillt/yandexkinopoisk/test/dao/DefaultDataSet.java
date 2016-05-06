9f538e49471ea57de5875912b548ae839b6bbf1f
Kirill <timofeev.kirill.a@gmail.com>
2016-05-06 22:41:55 +0300

    singleton!

--- src/test/java/im/kirillt/yandexkinopoisk/test/dao/DefaultDataSet.java
+++ src/test/java/im/kirillt/yandexkinopoisk/test/dao/DefaultDataSet.java
@@ -9,13 +9,22 @@ public class DefaultDataSet {
     public static final String COLUMN_ID = "ID";
     public static final String COLUMN_NAME = "NAME";
     public static final String COLUMN_AGE = "AGE";
+    private static DataSetBuilder builder = null;
+
+    public static DataSetBuilder getDefaultBuilder() throws DataSetException {
+        if (builder == null) {
+            builder = new DataSetBuilder();
+        }
+        return builder;
+    }
 
     static IDataSet getDataSet() throws DataSetException {
-        DataSetBuilder builder = new DataSetBuilder();
+        if (builder == null) {
+            builder = getDefaultBuilder();
+        }
         builder.newRow(TABLE_NAME).with(COLUMN_ID, 1).with(COLUMN_NAME, "Bob").with(COLUMN_AGE, 18).add();
         builder.newRow(TABLE_NAME).with(COLUMN_ID, 2).with(COLUMN_NAME, "Alice").with(COLUMN_AGE, 23).add();
         builder.newRow(TABLE_NAME).with(COLUMN_ID, 3).with(COLUMN_NAME, "Charlie").with(COLUMN_AGE, 42).add();
         return builder.build();
-
     }
 }
