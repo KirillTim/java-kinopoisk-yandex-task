acde142ed17200b3f76d7a6cd262a80917b41d01
Kirill <timofeev.kirill.a@gmail.com>
2016-05-06 21:34:21 +0300

    add setters and builders for Person

--- src/test/java/im/kirillt/yandexkinopoisk/test/dao/Person.java
+++ src/test/java/im/kirillt/yandexkinopoisk/test/dao/Person.java
@@ -12,4 +12,79 @@ public class Person {
     @Column(name = "last_Name") String lastName;
     @Column(name = "age") int age;
     public Person() {}
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public static final class PersonBuilder {
+        int age;
+        int id;
+        String name;
+        String lastName;
+
+        private PersonBuilder() {
+        }
+
+        public static PersonBuilder aPerson() {
+            return new PersonBuilder();
+        }
+
+        public PersonBuilder withAge(int age) {
+            this.age = age;
+            return this;
+        }
+
+        public PersonBuilder withId(int id) {
+            this.id = id;
+            return this;
+        }
+
+        public PersonBuilder withName(String name) {
+            this.name = name;
+            return this;
+        }
+
+        public PersonBuilder withLastName(String lastName) {
+            this.lastName = lastName;
+            return this;
+        }
+
+        public Person build() {
+            Person person = new Person();
+            person.setAge(age);
+            person.setId(id);
+            person.setName(name);
+            person.setLastName(lastName);
+            return person;
+        }
+    }
 }
